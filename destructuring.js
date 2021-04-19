f3703e11ae437588ea4a98e6ac3c394a04e1f229
Mutanium <info@gaetan.nl>
2021-04-19 22:35:10 +0200

    opdracht 1 t/m 4

--- destructuring.js
+++ destructuring.js
@@ -5,6 +5,9 @@ const product = {
   refreshRate: 50,
   screenType: 'LED',
 }
+const {price, refreshRate} = product;
+console.log(price);
+console.log(refreshRate);
 
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
@@ -17,6 +20,9 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+const {type, brand} = productInformation.general;
+console.log(type);
+console.log(brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,14 +44,24 @@ const tvOptions = {
   },
 };
 
+const {wifi, bluetooth} = tvOptions.options.connectivity.popular;
+console.log(wifi);
+console.log(bluetooth);
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
+const [first, second] = connectivity;
+console.log(first);
+console.log(second);
 
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
-const tvSizes = [41, 43, 46, 56, 65];
+//const tvSizes = [41, 43, 46, 56, 65];
+//const [1, 2, 3, 4, 5] = tvSizes;
+//console.log(1);
+//console.log(3);
 
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
