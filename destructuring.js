7cdbf25b6bd995a27cced9afa3265f08ab93d3b9
Mutanium <info@gaetan.nl>
2021-04-20 00:04:19 +0200

    opdracht 5 t/m 7

--- destructuring.js
+++ destructuring.js
@@ -1,5 +1,5 @@
 // Opdracht 1: Destructure price en refreshRate uit onderstaand object, en log ze daarna in de terminal
-
+console.log("Opdracht 1: ")
 const product = {
   price: 379,
   refreshRate: 50,
@@ -10,7 +10,7 @@ console.log(price);
 console.log(refreshRate);
 
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
-
+console.log("Opdracht 2: ")
 const productInformation = {
   general: {
     type: '43PUS6504/12',
@@ -25,7 +25,7 @@ console.log(type);
 console.log(brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
-
+console.log("Opdracht 3: ")
 const tvOptions = {
   options: {
     colors: [
@@ -49,7 +49,7 @@ console.log(wifi);
 console.log(bluetooth);
 
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
-
+console.log("Opdracht 4: ")
 const connectivity = ['wifi', 'bluetooth'];
 const [first, second] = connectivity;
 console.log(first);
@@ -57,24 +57,31 @@ console.log(second);
 
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
-
-//const tvSizes = [41, 43, 46, 56, 65];
-//const [1, 2, 3, 4, 5] = tvSizes;
-//console.log(1);
-//console.log(3);
+console.log("Opdracht 5: ")
+const tvSizes = [41, 43, 46, 56, 65];
+const [sizeOne, sizeTwo, sizeThree, sizeFour,] = tvSizes;
+console.log(sizeOne);
+console.log(sizeThree);
 
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
-
+console.log("Opdracht 6: ")
 function getCompanyDetails() {
   return {
     name: 'Novi Hogeschool',
     adress: 'Zonnebaan 9, Utrecht',
   }
 }
+const {name, adress} = getCompanyDetails();
+console.log(name);
+console.log(adress);
+
 
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
-
+console.log("Opdracht 7: ")
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+const [yourName, yourAge] = getDetails("Gaetan",51)
+console.log(yourName);
+console.log(yourAge);
